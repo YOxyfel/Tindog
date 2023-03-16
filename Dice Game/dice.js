0f84de0ed45e335f48512dea7f8890d99150534b
YOxyfel <125958938+YOxyfel@users.noreply.github.com>
2023-03-16 13:30:59 +0200

    Dice Game Added

---
+++ Dice Game/dice.js
@@ -0,0 +1,32 @@
+function txtChange(x)
+{
+    x.innerHTML = "Yes?";
+}
+
+function txtRevert(x)
+{
+    x.innerHTML = "Quick Dice Game?";
+}
+
+
+var randomNum = Math.floor(Math.random() * 6) + 1;
+var randomNum2 = Math.floor(Math.random() * 6) + 1;
+
+var imageNum = "images/dice" + randomNum + ".png";
+var imageNum2 = "images/dice" + randomNum2 + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src", imageNum);
+document.querySelectorAll("img")[1].setAttribute("src", imageNum2);
+
+if(randomNum > randomNum2)
+{
+    document.querySelector("h1").innerText = "Player One Wins 🚩";
+}
+else if(randomNum < randomNum2)
+{
+    document.querySelector("h1").innerText = "Player Two Wins 🚩";
+}
+else
+{
+    document.querySelector("h1").innerText = "Draw";
+}
