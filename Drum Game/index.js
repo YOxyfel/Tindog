9c0cdee0d454cddb96ae17046128a3a28d4c10d2
YOxyfel <125958938+YOxyfel@users.noreply.github.com>
2023-03-18 11:24:17 +0200

    Visual features

--- Drum Game/index.js
+++ Drum Game/index.js
@@ -3,8 +3,10 @@ var pressAmount = 0;
 
 for (var index = 0; index < drumSize; index++) {
     
-    document.querySelectorAll(".drum")[index].addEventListener("click", function ()
+    document.querySelectorAll(".drum")[index].addEventListener("click", function (e)
     {
+        
+        lightUp(e.key);
         exppr(this.innerHTML);
     }
     );
@@ -26,52 +28,56 @@ function checkStrokes()
     }
 }
 
+function keyAnimation(expr)
+{
+    var pressed = document.querySelector("." + expr).classList;
+    pressed.toggle("pressed");
+
+    setTimeout( function() {pressed.toggle("pressed"); }, 50);
+}
+
+
 
 function exppr(expr)
 {
     pressAmount++;
+    checkStrokes();
+    keyAnimation(expr);
+
     switch (expr) 
     {
         case 'w': 
             var audio = new Audio('sounds/crash.mp3');
                 audio.play();
-                checkStrokes();
             break;
         case 'a':
             var audio = new Audio('sounds/kick-bass.mp3');
                 audio.play();
-                checkStrokes();
             break;
         case 's':
             var audio = new Audio('sounds/snare.mp3');
                 audio.play();
-                checkStrokes();
             break;
         case 'd':
             var audio = new Audio('sounds/tom-1.mp3');
                 audio.play();
-                checkStrokes();
             break;
         case 'j':
             var audio = new Audio('sounds/tom-2.mp3');
                 audio.play();
-                checkStrokes();
             break;
         case 'k':
             var audio = new Audio('sounds/tom-3.mp3');
                 audio.play();
-                checkStrokes();
             break;
         case 'l':
             var audio = new Audio('sounds/tom-4.mp3');
                 audio.play();
-                checkStrokes();
             break;
         default:
         console.log(`Sorry, we are out of ${expr}.`);
         break;
     }
-
 }
   
 
